9be66cef2c023281c26ebab798c0bbbcab3f612a
d pullaiah <pullaiah.dudekula@improve10x.com>
2022-12-06 11:19:50 +0530

    craeted a new project CRUD

---
+++ app/src/main/java/com/improve10x/crud/SplashScreen.java
@@ -0,0 +1,23 @@
+package com.improve10x.crud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(this, DashboardActivity.class);
+            finish();
+            startActivity(intent);
+        }, 3000);
+    }
+}
