6c917e97f034aff0ed5ea185b3d7b85234ab882d
d pullaiah <pullaiah.dudekula@improve10x.com>
2022-12-15 15:41:13 +0530

    changed id phonenumber_txt to phone_number_txt in initValues method in BaseAddEditMessagesActivity class

--- app/src/main/java/com/improve10x/crud/messages/BaseAddEditMessagesActivity.java
+++ app/src/main/java/com/improve10x/crud/messages/BaseAddEditMessagesActivity.java
@@ -25,8 +25,7 @@ public class BaseAddEditMessagesActivity extends BaseActivity {
 
     private void initValues() {
         nameTxt = findViewById(R.id.name_txt);
-        // todo ; change the id to phone_number_txt
-        phoneNumberTxt = findViewById(R.id.phonenumber_txt);
+        phoneNumberTxt = findViewById(R.id.phone_number_txt);
         messageTextTxt = findViewById(R.id.message_text_txt1);
     }
 
